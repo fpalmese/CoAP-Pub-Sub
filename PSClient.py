f3aacd72dcffc176018a3b374007b567de5ced59
Fabio Palmese <fabiopalmese97@gmail.com>
2020-08-26 15:15:51 +0200

    Updated the PsClient
    Improved unsubscribe

--- PSClient.py
+++ PSClient.py
@@ -2,13 +2,14 @@ from coapthon.client.helperclient import HelperClient
 from coapthon.defines import Codes
 from coapthon.utils import generate_random_token
 from utils import generate_subscribe_token
-host = "127.0.0.1"
-port = 5683
 
 
+"""
+    Class that extends the coapthon HelperClient 
+    and allows to create a Publish-Subscribe CoAP client.
+"""
 
 class PSClient(HelperClient):
-	psPath="/ps"
 	observed_list = {}
 	#DISCOVERY with the DISCOVER method (a GET to ./well-known/core)
 	def discovery(self):
@@ -17,16 +18,16 @@ class PSClient(HelperClient):
 		response = self.discover()		
 		topicList=self.parseDiscover(response)
 		if response is not None:
-			print ("received response for DISCOVERY:\nTopic List: "+str(topicList)+"\nPS: "+self.psPath)
+			print ("received response for DISCOVERY:\nTopic List: "+str(topicList)+"\n")
 		else:
 			print("received empty response")
 		print("------------------------")
 
 	#CREATE method handled on POST
 	def create(self,path,topicName,topicCT):
-		print("sending CREATE for "+self.psPath+path+"/"+topicName)
+		print("sending CREATE for "+path+"/"+topicName)
 		payload ="<"+topicName+">;ct="+str(topicCT)+";"
-		response = self.post("/ps"+path,payload)
+		response = self.post(path,payload)
 		#print response.pretty_print()
 		if response is not None:
 			self.printResponse(response)
@@ -39,10 +40,10 @@ class PSClient(HelperClient):
 		print("PUBLISH on "+topic+ ": "+payload)
 		
 		if(method.lower()=="post"):
-			response = self.post(self.psPath+topic, payload)
+			response = self.post(topic, payload)
 			
-		else:
-			response = self.put(self.psPath+topic, payload)
+		elif(method.lower()=="put"):
+			response = self.put(topic, payload)
 		#print(response.pretty_print())
 		self.printResponse(response)
 		print("------------------------")
@@ -50,41 +51,47 @@ class PSClient(HelperClient):
 	def subCallback(self,message):
 		if message is not None:
 			print("Received message for subscription:\nCode: "+Codes.LIST[message.code].name+"\nPayload: "+str(message.payload))
+			if message.code == Codes.DELETED.number or message.code == Codes.NOT_FOUND.number:
+				topic = message.payload.split(" ")[0]
+				self.subThreads[topic].stopit()
+				del self.subThreads[topic]
+				
+
 		else:
 			print("Received empty message")
 		print("------------------------")
+		return
 
 	#SUBSCRIBE handled on OBSERVE (GET with observe = 0)
 	def subscribe(self,topic):
-		print("SUBSCRIBE to "+ topic)
+		print("SUBSCRIBE to "+topic)
 		tkn_size = 4	#token size in bytes
 		token = generate_subscribe_token(topic,tkn_size)
-		response = self.observe(self.psPath+"/"+topic,self.subCallback,token)
-		print("------------------------")
+		response = self.observe(topic,self.subCallback,token)
 	
-	#UNSUBSCRIBE handled on GET with observe = 1
+	#UNSUBSCRIBE handled on REMOVE_OBSERVE (GET with observe = 1)
 	def unsubscribe(self,topic):
-		print("UNSUBSCRIBE to "+ topic)
+		print("UNSUBSCRIBE to "+topic)
+		if topic not in self.subThreads:
+			print("Cannot send unsubscribe to a not subscribed resource")
+			print("--------------------------")
+			return
+		#self.subThreads[topic].stopit()	
 		tkn_size = 4 #token size in bytes
 		token = generate_subscribe_token(topic,tkn_size)
-		response = self.remove_observe(self.psPath+"/"+topic,token)
-		self.printResponse(response)
-		print("------------------------")
+		response = self.remove_observe(topic,token)
 
 	#function to parse the response to discovery
 	def parseDiscover(self,response):	#take topic list and update the ps
 						#PROBLEM IN THIS FUNCTION HERE WHEN WE SET THE PSPATH
 		topicList=str(response.payload).split(",")
 		topicList.pop()
-		for t in topicList:
-			if("core.ps" in t):
-				self.psPath=[s.split('>')[0] for s in t.split('<') if '>' in s][0]
 		return topicList
 
 	#function for simple print of response code and payload
 	def printResponse(self,response):
 		try:
-			print("Received Response: \n Code: "+Codes.LIST[response.code].name+"\nPayload: "+response.payload)
+			print("Received Response: \n Code: "+Codes.LIST[response.code].name+"\nPayload: "+str(response.payload))
 		except:
 			print("Received malformed response")
 
