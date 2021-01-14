78fe21224ee91caabf74ceff58603219229f8fb2
Fabio Palmese <fabiopalmese97@gmail.com>
2021-01-14 17:30:51 +0100

    Removed unneeded files

--- comparison_coap/for_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {1..1}
-do
-   sudo bash run.sh 100
-   sleep 2
-done
-
--- cpuMonitor.py
+++
@@ -1,7 +0,0 @@
-import os,time
-f = open("csv/pid.txt")
-pid = int(f.readline())
-#os.system("ps -p "+pid+" -o %cpu,%mem --no-headers >> csv/broker.csv")
-while(True):
-	os.system("ps -p "+str(pid)+" -o %cpu,%mem --no-headers >> csv/broker.csv")
-	time.sleep(0.5)
